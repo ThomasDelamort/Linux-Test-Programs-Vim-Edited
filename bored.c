f5161f5bb7ed93f333cb6a64926e72d514f2b9d8
__neallll__ <nealparedes776@gmail.com>
2026-06-27 12:50:35 +0800

    Add files via upload

--- bored.c
+++ bored.c
@@ -2,11 +2,32 @@
 
 int main () {
     int numberOfCourses = 0;
-
-
+    double gwa = 0;
+    double sum = 0;
+    double grade = 0;
 
     printf("<>===<> Calculate your GWA now <>===<>");
     
     printf("\nHow many courses: "); 
+    scanf("%d", &numberOfCourses); 
+    double grades[numberOfCourses];
+
+    for (int i = 0; i < numberOfCourses; i++) {
+	printf("Enter Grade %d: ", i+1);
+        scanf(" %lf", &grade);
+	grades[i] = grade;
+	grade = 0;
+    }
+
+    for (int j = 0; j < numberOfCourses; j++) {
+	sum += grades[j];
+    }
+    gwa = sum / numberOfCourses;
+
+    printf("\nYour GWA is: %.2f", gwa);
     return 0;
+
+    if (gwa < 2.5) {
+	printf("Very Good!");
+    }
 }
